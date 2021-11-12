b5391adb794984a8c5c5a965d260a198cac13afd
smahkafi <sakafi.m@gmail.com>
2021-11-13 01:17:02 +0600

    footer designed

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../../img/logo-footer-img.webp'
 
@@ -11,37 +11,47 @@ const Footer = () => {
                     <div className="col-sm-6 col-md-6 col-lg-3 align-self-center">
                         <img className="" src={logo} alt="" />
                     </div>
-                    <div className="col-sm-6 col-md-6 col-lg-3">
-                        <h2>SERVICES</h2>
-                        <Link className="text-decoration-none">
-
-                            <p>Book eye test</p>
-                            <p>Vision therapy</p>
-                            <p>Vouchers</p>
-                            <p>Lenses & prices</p>
-                            <p>Eye health</p>
+                    <div className="col-sm-6 col-md-6 col-lg-3 text-start">
+                        <h4 className="text-white fw-normal">Contact Us</h4>
+                        <Link to="/" className="text-decoration-none">
+                            <p className="text-white">  451 Wall Street, UK, London</p>
+                        </Link>
+                        <Link to="/" className="text-decoration-none">
+                            <p className="text-white">Phone: (+084) 333-1233</p>
+                        </Link>
+                        <Link to="/" className="text-decoration-none">
+                            <p className="text-white">demo@demo.com</p>
                         </Link>
                     </div>
-                    <div className="col-sm-6 col-md-6 col-lg-3">
-                        <h2>SERVICES</h2>
-                        <Link className="text-decoration-none">
-
-                            <p>Book eye test</p>
-                            <p>Vision therapy</p>
-                            <p>Vouchers</p>
-                            <p>Lenses & prices</p>
-                            <p>Eye health</p>
+                    <div className="col-sm-6 col-md-6 col-lg-3 text-start">
+                        <h4 className="text-white fw-normal">DESIGN BLOG</h4>
+                        <Link to="/" className="text-decoration-none text-white">
+                            <p>Blog</p>
+                        </Link>
+                        <Link to="/" className="text-decoration-none text-white">
+                            <p>Forums</p>
+                        </Link>
+                        <Link to="/" className="text-decoration-none text-white">
+                            <p>Builders Showcase</p>
+                        </Link>
+                        <Link to="/" className="text-decoration-none text-white">
+                            <p>How-To Guides</p>
                         </Link>
                     </div>
-                    <div className="col-sm-6 col-md-6 col-lg-3">
-                        <h2>SERVICES</h2>
-                        <Link className="text-decoration-none">
-                            <p>Book eye test</p>
-                            <p>Vision therapy</p>
-                            <p>Vouchers</p>
-                            <p>Lenses & prices</p>
-                            <p>Eye health</p>
-                        </Link>
+                    <div className="col-sm-6 col-md-6 col-lg-3 text-start">
+                        <h4 className="text-white fw-normal">SIGN UP FOR EMAIL</h4>
+                        <form>
+                            <p className="text-white fw-normal p-0 m-0">Send me obliges news, updates and offers.</p>
+                            <div className="">
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <label class="form-check-label text-white" for="exampleCheck1">Check me out</label>
+                                <div id="emailHelp" class="form-text text-white">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div className="form-check">
+                                <Button type="submit" className="btn-primary">Submit</Button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </Container>
